23627f4d257168d42428db1e0930322fa6d01066
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-05-12 10:28:46 +0200

    add module EmptyCart

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,12 +1,14 @@
 import { useDocumentTitle } from "../../shared/hooks/useDocumentTitle";
 import Container from "../../shared/components/Container/Container";
 import SectionTitle from '../../shared/components/SectionTitle/SectionTitle'
-const CartPage = () =>{
-    useDocumentTitle('Cart');
-    return(
-        <Container>
-        <SectionTitle title='Shopping cart' linkTo='/categories' linkText='Back to the store'/>
+import EmptyCart from "../../modules/EmptyCart/EmptyCart";
 
+const CartPage = () => {
+    useDocumentTitle('Cart');
+    return (
+        <Container>
+            <SectionTitle title='Shopping cart' linkTo='/categories' linkText='Back to the store' />
+            <EmptyCart />
         </Container>
     )
 }
