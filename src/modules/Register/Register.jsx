595ee53be55ee114b43568fa0b581e20ab87da7e
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-05-15 22:38:52 +0200

    changed image to background-image

--- src/modules/Register/Register.jsx
+++ src/modules/Register/Register.jsx
@@ -1,12 +1,11 @@
 import RegisterForm from "./RegisterForm/RegisterForm";
-import Container from "../../shared/components/Container/Container";
 import styles from './Register.module.css'
 const Register = () => {
     return (< >
         <div className={styles.container}>
             <h3 className={styles.title}>5% off on the first order</h3>
             <div className={styles.flex}>
-                <img src="src\assets\images\fotoRegForm.png" alt="anim" className={styles.img} />
+                <div className={styles.img}></div>
                 <RegisterForm className={styles.form} />
             </div></div>
     </>)
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
     return (
         <Container>
             <div className={styles.flex}>
-                <img src="src\assets\images\404.png" alt="404" className={styles.img} />
+                <div className={styles.img} ></div>
                 <h1 className={styles.title}>Page Not Found</h1>
                 <p className={styles.text}>We’re sorry, the page you requested could not be found.</p>
                 <p className={styles.text}>Please go back to the homepage.</p>
